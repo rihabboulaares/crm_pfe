a36e5007bf1b643645ac8fee25589744682eff62
Rihab Boulaares <rihab.boulaares@esprit.tn>
2026-06-11 15:22:25 +0100

    Register CRM models in Django admin

--- sales/admin.py
+++ sales/admin.py
@@ -1,19 +1,20 @@
 from django.contrib import admin
-from .models import PerformanceScore, ManagerFeedback, PerformanceGoal, CommercialBadge
-
-@admin.register(PerformanceScore)
-class PerformanceScoreAdmin(admin.ModelAdmin):
-    list_display = ["commercial", "year", "month", "score", "penalty_points"]
-    list_filter  = ["year", "month"]
-
-@admin.register(ManagerFeedback)
-class ManagerFeedbackAdmin(admin.ModelAdmin):
-    list_display = ["given_by", "commercial", "rating", "year", "month"]
-
-@admin.register(PerformanceGoal)
-class PerformanceGoalAdmin(admin.ModelAdmin):
-    list_display = ["commercial", "goal_type", "current_value", "target_value", "status"]
-
-@admin.register(CommercialBadge)
-class CommercialBadgeAdmin(admin.ModelAdmin):
-    list_display = ["commercial", "badge_type", "year", "month"]
+from .models import (
+    Account, ProspectCompany, Prospect, Contact,
+    Opportunity, Task, TaskActivity, TaskComment,
+    Pipeline, PipelineStage, OpportunityPipeline,
+    PipelineAlert, PipelineStageTask,
+)
+admin.site.register(Account)
+admin.site.register(ProspectCompany)
+admin.site.register(Prospect)
+admin.site.register(Contact)
+admin.site.register(Opportunity)
+admin.site.register(Task)
+admin.site.register(TaskActivity)
+admin.site.register(TaskComment)
+admin.site.register(Pipeline)
+admin.site.register(PipelineStage)
+admin.site.register(OpportunityPipeline)
+admin.site.register(PipelineAlert)
+admin.site.register(PipelineStageTask)
--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import SubscriptionPlan, CompanySubscription
 
-# Register your models here.
+admin.site.register(SubscriptionPlan)
+admin.site.register(CompanySubscription)
